b35b309ab75ab8b96ecddf6a92cd91b4d3e265ea
Jeff <jeffrey.arthur.daniels@gmail.com>
2020-06-13 13:44:45 +0000

    complete DecoderRNN

--- Project 2: Image Captioning/model.py
+++ Project 2: Image Captioning/model.py
@@ -23,11 +23,47 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        # embedding layer that converts captions into a vector of a specified size
+        self.caption_embeddings_layer = nn.Embedding(vocab_size, embed_size)
+
+        # the LSTM takes embedded word vectors (of a specified size) as inputs 
+        # and outputs hidden states of size hidden_dim
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+
+        # the linear layer that maps the hidden state output dimension 
+        # to the number of words we want as output, vocab_size
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        ''' Define the feedforward behavior of the model.'''
+        
+        # pass captions through an embedding layer
+        # length in words of each caption == captions.shape[1]
+        # embeds.shape = (batch_size, captions.shape[1], embed_size)
+        captions = captions[:, :-1]
+        caption_embeddings = self.caption_embeddings_layer(captions)
+        
+        # Concatenate features with the caption embeddings
+        # features.shape == (batch_size, embed_size)
+        # caption_embeddings.shape == (batch_size, captions.shape[1], embed_size) 
+        # inputs.shape == (batch_size, captions.shape[1], embed_size)
+        inputs = torch.cat((features.unsqueeze(1), caption_embeddings), dim=1)
+
+        # get the output and hidden state by passing the lstm over our word embeddings
+        # the lstm takes in our embeddings and hidden state
+        lstm_out, _ = self.lstm(inputs)
+        outputs = self.linear(lstm_out)
+        
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        sentance = []
+        for w in range(max_len):
+            sentance.append(self.forward(inputs))
+        return sentance
