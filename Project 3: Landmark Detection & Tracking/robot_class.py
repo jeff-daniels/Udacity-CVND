e747737528807e275ae500e07ae1db4aedc4d102
Jeff <jeffrey.arthur.daniels@gmail.com>
2020-06-23 10:43:36 +0000

    implement sense function

--- Project 3: Landmark Detection & Tracking/robot_class.py
+++ Project 3: Landmark Detection & Tracking/robot_class.py
@@ -65,11 +65,9 @@ class robot:
     #        landmarks to be visible at all times
     #
     
-    ## TODO: paste your complete the sense function, here
-    ## make sure the indentation of the code is correct
     def sense(self):
         ''' This function does not take in any parameters, instead it references internal variables
-            (such as self.landamrks) to measure the distance between the robot and any landmarks
+            (such as self.landmarks) to measure the distance between the robot and any landmarks
             that the robot can see (that are within its measurement range).
             This function returns a list of landmark indices, and the measured distances (dx, dy)
             between the robot's position and said landmarks.
@@ -77,7 +75,29 @@ class robot:
             One item in the returned list should be in the form: [landmark_index, dx, dy].
             '''
            
-        measurements = None
+        measurements = []
+        
+        ## iterate through all of the landmarks in a world
+        for idx, landmark in enumerate(self.landmarks):
+            ## For each landmark
+            ## 1. compute dx and dy, the distances between the robot and the landmark
+            ## 2. account for measurement noise by *adding* a noise component to dx and dy
+            ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
+            ##    - Feel free to use the function self.rand() to help calculate this noise component
+            ##    - It may help to reference the `move` function for noise calculation
+            
+            dx = self.x - landmark[0] + self.rand() * self.measurement_noise
+            dy = self.y - landmark[1] + self.rand() * self.measurement_noise            
+            
+            ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
+            ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
+            ##    as list.append([index, dx, dy]), this format is important for data creation done later
+            if (abs(dx) < self.measurement_range) and (abs(dy) < self.measurement_range):
+                measurements.append([idx, dx, dy])
+            
+        ## return the final, complete list of measurements
+        return measurements
+
         
         ## TODO: iterate through all of the landmarks in a world
         
