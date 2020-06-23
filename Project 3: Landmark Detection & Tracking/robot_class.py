7a0f75774cd8f23871b8c76969a39ef1615d4183
Jeff <jeffrey.arthur.daniels@gmail.com>
2020-06-23 15:42:10 +0000

    get slam function to be accurate

--- Project 3: Landmark Detection & Tracking/robot_class.py
+++ Project 3: Landmark Detection & Tracking/robot_class.py
@@ -98,21 +98,6 @@ class robot:
         ## return the final, complete list of measurements
         return measurements
 
-        
-        ## TODO: iterate through all of the landmarks in a world
-        
-        ## TODO: For each landmark
-        ## 1. compute dx and dy, the distances between the robot and the landmark
-        ## 2. account for measurement noise by *adding* a noise component to dx and dy
-        ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
-        ##    - Feel free to use the function self.rand() to help calculate this noise component
-        ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
-        ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
-        ##    as list.append([index, dx, dy]), this format is important for data creation done later
-        
-        ## TODO: return the final, complete list of measurements
-        return measurements
-
 
     # --------
     # make_landmarks:
